e3b05d6336d2ae3a46dfb3646b617e176457d43a
s-songma <s-songma@53S500036556.it.bsd405.org>
2019-11-21 22:10:24 -0800

    Checkpoint 2!!

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -11,9 +11,18 @@ public class FracCalc {
 	public static void main(String[] args) {
 		System.out.println("This is the Fraction Calculator.");
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Enter an expression, or type \"quit\" to quit the program.");
-		String input = userInput.nextLine();
-		System.out.println("Value 2: " + produceAnswer(input));
+		boolean run = true;
+		while (run) {
+			System.out.println("Enter an expression, or type \"quit\" to quit the program.");
+			String inputFake = userInput.nextLine();
+			String input = inputFake.toLowerCase();
+			if (!input.equals("quit")) {
+				System.out.println(produceAnswer(input));
+			} else {
+				System.out.println("You have chosen to quit this program. Bye.");
+				run = false;
+			}
+		}
 	}
 
 	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
@@ -30,14 +39,14 @@ public class FracCalc {
 	public static String produceAnswer(String input) {
 		int idx = input.indexOf(' ');
 		String val1 = input.substring(0, idx);
-		System.out.println("Value 1: " + val1);
+		// System.out.println("Value 1: " + val1);
 
 		String remaining = input.substring(idx + 1);
 		// System.out.println("Remaining: " + remaining);
 
 		idx = remaining.indexOf(' ');
 		String op = remaining.substring(0, idx);
-		System.out.println("Operator: " + op);
+		// System.out.println("Operator: " + op);
 
 		remaining = remaining.substring(idx + 1);
 		// System.out.println("Remaining: " + remaining);
@@ -48,34 +57,53 @@ public class FracCalc {
 		//
 		// Parse Value 1 into parts
 		//
-//		idx = val1.indexOf('_');
-//		if (idx != -1) {
-//			String w1 = val1.substring(0, idx);
-//			 System.out.println("W1: " + w1);
-//			val1 = val1.substring(idx + 1);
-//		}
-//		idx = val1.indexOf('/');
-//		int n1 = Integer.parseInt(val1.substring(0, idx));
-//		int d1 = Integer.parseInt(val1.substring(idx + 1));
-//		 System.out.println("N1: " + n1 + " D1: " + d1);
-
+		idx = val1.indexOf('_');
+		if (idx != -1) {
+			String w1 = val1.substring(0, idx);
+			// System.out.println("W1: " + w1);
+			val1 = val1.substring(idx + 1);
+			idx = val1.indexOf("/");
+			int n1 = Integer.parseInt(val1.substring(0, idx));
+			int d1 = Integer.parseInt(val1.substring(idx + 1));
+			// System.out.println("N1: " + n1 + " D1: " + d1);
+			String printOne = "whole:" + w1 + "numerator" + n1 + "denominator" + d1;
+		} else {
+			idx = val1.indexOf('/');
+			if (idx != -1) {
+				int n1 = Integer.parseInt(val1.substring(0, idx));
+				int d1 = Integer.parseInt(val1.substring(idx + 1));
+				// System.out.println("numerator: " + n1 + " denominator: " + d1);
+			} else {
+				String printOne = "whole:" + val1 + "numerator:0" + "denominator:1";
+			}
+		}
 		//
 		// Parse Value 2 into parts
 		//
-//		idx = val2.indexOf('_');
-//	    if (idx!=-1) {
-//	      String w2 = val2.substring(0, idx);
-//	      System.out.println("W2: " + w2);
-//	      val2 = val2.substring(idx + 1);
-//	    }
-//	    idx = val2.indexOf('/');
-//	    int n2 = Integer.parseInt(val2.substring(0, idx));
-//	    int d2 = Integer.parseInt(val2.substring(idx + 1));
-//	    System.out.println("N2: " + n2 + " D2: " + d2);
-		return val2;
+		idx = val2.indexOf('_');
+		if (idx != -1) {
+			String w2 = val2.substring(0, idx);
+			val2 = val2.substring(idx + 1);
+			idx = val2.indexOf("/");
+			int n2 = Integer.parseInt(val2.substring(0, idx));
+			int d2 = Integer.parseInt(val2.substring(idx + 1));
+			String printTwo = "whole:" + w2 + " numerator:" + n2 + " denominator:" + d2;
+			return printTwo;
+
+		} else {
+			idx = val2.indexOf('/');
+			if (idx != -1) {
+				int n2 = Integer.parseInt(val2.substring(0, idx));
+				int d2 = Integer.parseInt(val2.substring(idx + 1));
+				String printTwo = "whole:0" + " numerator:" + n2 + " denominator:" + d2;
+				return printTwo;
+			} else {
+				String printTwo = "whole:" + val2 + " numerator:0" + " denominator:1";
+				return printTwo;
+			}
+		}
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
-
 }
